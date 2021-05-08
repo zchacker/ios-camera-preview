861090f915e7d5462e3889f2ee7963abb9a41fb3
Ahmed Adm <softwaretest48@gmail.com>
2021-05-08 20:50:22 +0300

    Initial Commit

---
+++ camera/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  camera
+//
+//  Created by Ahmed Adm on 26/09/1442 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
